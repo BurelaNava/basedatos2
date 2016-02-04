0fe8e8614314a581384e959165ac3154f10b8d99
Axel Burela <buremmstein@gmail.com>
2016-02-03 21:45:12 -0600

    Update practica1.sql

--- practica1.sql
+++ practica1.sql
@@ -85,6 +85,29 @@ dbms_output.put_line('La suma es ' ||valor);
 end;
 /
 
+CREATE OR REPLACE PROCEDURE GUARDAR_PELICULA 
+(
+  MY_ID_PELICULA OUT NUMBER
+, MY_TITULO IN VARCHAR2  
+, MY_SINOPSIS IN VARCHAR2  
+, MY_CLASIFICACION IN VARCHAR2  
+) AS 
+BEGIN
+SELECT SEC_PELICULA.NEXTVAL INTO MY_ID_PELICULA FROM DUAL;
+INSERT INTO PELICULA VALUES(MY_ID_PELICULA, MY_TITULO, MY_SINOPSIS, MY_CLASIFICACION);
+
+
+END GUARDAR_PELICULA;
+/
+
+declare
+valor integer;
+begin
+GUARDAR_PELICULA(valor,'El renacido','Está, pues más o menos','B15');
+end;
+/
+select * from pelicula;
+
 
 
           
